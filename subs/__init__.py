32084b4bf3a8c4ad8192ab47aa0704fb3fa9161b
Steffan P <stp1g15@soton.ac.uk>
2018-01-18 18:17:50 +0000

    should have a working logger now

--- subs/__init__.py
+++ subs/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from google.appengine.api.app_logging import AppLogsHandler
+
 
 app = Flask(__name__)
 app.secret_key = 'Y!m&#!s#JW7Qi2q@wVTcFg4dpOvlCuGn$JJMv^D@HUU754K@03'
+app.logger.addHandler(AppLogsHandler())
 
 from subs import views
